74ed8ea26f674acb7d78f1b0b6418cc1af822e91
Иван <kleimenov416@gmail.com>
2021-03-11 10:22:27 -0500

    implement simplified Redux wersion

--- src/createStore.js
+++ src/createStore.js
@@ -1,15 +1,22 @@
-export const createStore = () => {
+export const createStore = (rootReducer) => {
+  //implement closures and observer pattern and it will 
 
-    //implement closures
+  let state = {};
+  const subscribers = [];
 
-    return {
-        dispatch() {
+  return {
+    dispatch(action) {
+      //action = specific object {type: 'INCREMENT'}
+      state = rootReducer(state, action);
+      subscribers.forEach((sub) => sub());
+    },
 
-        },
-        subscribe() {
-            
-        },
-        getState()
+    subscribe(callback) {
+      subscribers.push(callback);
+    },
 
-    }
-}
+    getState() {
+      return state;
+    },
+  };
+};
