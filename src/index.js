b2c20da7f3daa098e00605d0c1e52867e659c489
Иван <kleimenov416@gmail.com>
2021-03-11 09:26:18 -0500

    create state (it represents our current state) and counter (it is our model), also implement render function that connect our current state and model

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './styles.css'
+import "./styles.css";
+
+const counter = document.getElementById("counter");
+
+let state = 0;
+
+//let's connect our representation whith state
+function render() {
+  counter.textContent = state.toString();
+}
+
+//invoke render function
+render();
