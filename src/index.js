2081a025a847294aed02fe0292f0afdd6d9beab5
Иван <kleimenov416@gmail.com>
2021-03-11 09:56:54 -0500

    create a simplified React model

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ const colorScheme = document.getElementById("theme");
 
 let state = 0;
 
-//let's connect our representation whith state
+//let's connect our representation whith state (in this case, render function works like a simplified React model)
 function render() {
   counter.textContent = state.toString();
 }
+
 
 add.addEventListener("click", () => {
     state++;
@@ -21,6 +22,12 @@ sub.addEventListener("click", () => {
     state--;
   render();
 });
+
+colorScheme.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark')
+    colorScheme.classList.toggle('btn-info')
+    colorScheme.classList.toggle('btn-custom')
+})
 
 //invoke render function
 render();
